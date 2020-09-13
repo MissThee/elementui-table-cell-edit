f4fccca76e9dcb87bd9044ec89cd260020f02bbc
MT <1163182175@qq.com>
2020-09-13 22:15:06 +0800

    更新预览样式

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
-    <table-cell-edit/>
+    <div style="width:800px;margin:auto;">
+      <div style="border:1px solid #ccc;padding:10px">
+        <table-cell-edit/>
+      </div>
+      <div>
+        <div>查看源码：</div>
+        <div><a href="https://github.com/MissThee/elementui-table-cell-edit">https://github.com/MissThee/elementui-table-cell-edit</a></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import TableCellEdit from "./TableCellEdit";
+
   export default {
     name: 'App',
     components: {TableCellEdit},
     data() {
-      return {
-
-      };
+      return {};
     },
   };
 </script>
